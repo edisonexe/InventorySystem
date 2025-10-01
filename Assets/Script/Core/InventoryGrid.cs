285b2d7538843d474395aae94fcd7e5e09ea03dc
ray_rid <rid843@mail.ru>
2025-10-01 15:13:48 +0700

    feat: add InventoryController and Grid

---
+++ Assets/Script/Core/InventoryGrid.cs
@@ -0,0 +1,75 @@
+﻿using Controllers;
+using Core;
+using Core.Input;
+using UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Script.Core
+{
+    public class InventoryGrid : MonoBehaviour
+    {
+        [SerializeField] private GridLayoutGroup _grid;
+        [SerializeField] private SlotView _slotPrefab;
+        [SerializeField] private DragIcon _dragIcon;
+        [SerializeField] private TooltipView _tooltip;
+
+        [SerializeField] private ItemConfig _demoWeapon1;
+        [SerializeField] private ItemConfig _demoWeapon2;
+        [SerializeField] private ItemConfig _demoPotion1;
+        [SerializeField] private ItemConfig _demoPotion2;
+        
+        private InventoryController _controller;
+        private SlotView[] _views;
+        
+        public DragIcon DragIcon => _dragIcon;
+        public TooltipView Tooltip => _tooltip;
+        public InventoryController Controller => _controller;  
+        
+        public void Build(InventoryController controller, int w, int h)
+        {
+            _controller = controller;
+            int count = w * h;
+            _views = new SlotView[count];
+            for (int i = 0; i < count; i++)
+            {
+                var view = Instantiate(_slotPrefab, _grid.transform);
+                view.Bind(i);
+                _views[i] = view;
+                
+                var input = view.GetComponent<SlotInput>();
+                if (input != null)
+                {
+                    input.Init(this, view);
+                }
+
+                RefreshSlot(i);
+            }
+        }
+        
+        public void RefreshSlot(int index)
+        {
+            if (_views == null || index < 0 || index >= _views.Length) return;
+            _views[index].Render(_controller.Model.Get(index));
+        }
+        
+        public void ShowDragIcon(InventoryItem item) => _dragIcon.Show(item);
+
+        public void HideDragIcon() => _dragIcon.Hide();
+        
+        public void PlayUseFeedback(int index) { }
+
+        public void DebugFillDemoItems()
+        {
+            var m = _controller.Model;
+            m.Set(0, new InventoryItem (_demoWeapon1, 1));
+            m.Set(1, new InventoryItem (_demoWeapon2, 10));
+            m.Set(2, new InventoryItem (_demoPotion1, 10));
+            m.Set(3, new InventoryItem (_demoPotion2, 10));
+            m.Set(4, new InventoryItem (_demoPotion2, 10));
+            m.Set(5, new InventoryItem (_demoWeapon2, 10));
+            m.Set(6, new InventoryItem (_demoPotion1, 10));
+            m.Set(7, new InventoryItem (_demoPotion1, 20));
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/InventoryController.cs
@@ -0,0 +1,98 @@
+﻿using Core;
+using Script.Core;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class InventoryController : MonoBehaviour
+    {
+        [Header("GridSize")]
+        [SerializeField] private int _width = 5;
+        [SerializeField] private int _height = 4;
+
+        [SerializeField] private InventoryGrid _gridView;
+        public InventoryModel Model {get; private set;}
+        
+        public int draggingFrom = -1;
+        private InventoryItem _draggingItem;
+        
+        private void Awake()
+        {
+            Model = new InventoryModel(_width, _height);
+            Model.OnSlotChanged += OnSlotChanged;
+            _gridView.Build(this, _width, _height);
+            _gridView.DebugFillDemoItems();
+        }
+
+        private void OnDestroy() => Model.OnSlotChanged -= OnSlotChanged;
+        private void OnSlotChanged(int index) => _gridView.RefreshSlot(index);
+        
+        public void BeginDrag(int index)
+        {
+            var item = Model.Get(index);
+            if (item.IsEmpty) return;
+            draggingFrom = index;
+            _draggingItem = item;
+            Model.Set(index, InventoryItem.Empty);
+            _gridView.ShowDragIcon(item);
+        }
+        
+        public void DropOn(int targetIndex)
+        {
+            if (draggingFrom < 0) return;
+            
+            if (targetIndex == draggingFrom)
+            {
+                Model.Set(draggingFrom, _draggingItem);
+                EndDrag();
+                return;
+            }
+
+            var target = Model.Get(targetIndex);
+
+            if (target.IsEmpty)
+            {
+                Model.Set(targetIndex, _draggingItem);
+                EndDrag();
+                return;
+            }
+            
+            if (target.Config == _draggingItem.Config && target.Stackable)
+            {
+                Model.TryMergeInternal(_draggingItem, targetIndex);
+                EndDrag();
+                return;
+            }
+
+            var tmp = target;
+            Model.Set(targetIndex, _draggingItem);
+            Model.Set(draggingFrom, tmp);
+
+            EndDrag();
+        }
+
+        public void CancelDrag()
+        {
+            if (draggingFrom >= 0 && !_draggingItem.IsEmpty)
+            {
+                Model.TryAddAt(draggingFrom, _draggingItem);
+            }
+            EndDrag();
+        }
+
+        private void EndDrag()
+        {
+            _gridView.HideDragIcon();
+            draggingFrom = -1;
+            _draggingItem = InventoryItem.Empty;
+        }
+
+        public void UseAt(int index)
+        {
+            if (Model.UseAt(index)) 
+                _gridView.PlayUseFeedback(index);
+        }
+
+        public void DropAt(int index) => Model.RemoveAt(index);
+    }
+}
